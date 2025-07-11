b346c07b860356df593bd44bf88b707b9fbbb7a4
Sebastian Hernandez Ubilla <sebastian.hernandez17@outlook.com>
2025-07-11 16:59:19 -0600

    Consume Api quotes is added

---
+++ src/features/quotes/quotesSlice.js
@@ -0,0 +1,36 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const fetchQuote = createAsyncThunk("quotes/fetchQuote", async () => {
+  const response = await axios.get(
+    "http://api.quotable.io/quotes/random?maxLength=150"
+  );
+  return response.data;
+});
+
+const quotesSlice = createSlice({
+  name: "quotes",
+  initialState: {
+    quote: {},
+    status: "idle",
+    error: null,
+  },
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchQuote.pending, (state) => {
+        state.status = "loading";
+      })
+      .addCase(fetchQuote.fulfilled, (state, action) => {
+        state.status = "fulfilled";
+        state.quote = action.payload;
+      })
+      .addCase(fetchQuote.rejected, (state, action) => {
+        state.status = state.error = action.error.message;
+      });
+  },
+});
+
+export const selectQuote = state => state.quotes;
+
+export const quotesReducer = quotesSlice.reducer;
